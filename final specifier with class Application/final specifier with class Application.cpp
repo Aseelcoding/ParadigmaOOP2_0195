4f5ae594f65154e2135dea9a45b591cf8816847b
Aseel <asilalhashmi@email.com>
2026-05-19 13:17:18 +0700

    make the baseclass the the keyword final it means you can not inherit from it

--- final specifier with class Application/final specifier with class Application.cpp
+++ final specifier with class Application/final specifier with class Application.cpp
@@ -1,6 +1,12 @@
 
 #include <iostream>
 using namespace std;
+class BaseClass final {
+public:
+    virtual void introduction() {
+        cout << "Hello I am a Function from base class";
+    }
+};
 int main()
 {
 
