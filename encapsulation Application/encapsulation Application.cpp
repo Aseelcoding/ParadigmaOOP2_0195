0d815561b0b16797045d49922323516ef0d1bb5d
Aseel <asilalhashmi@email.com>
2026-05-19 13:05:54 +0700

    make an opject and call the class funcrions . there is somthing important here the size of the string of array is 4 it means you cant put more than 4 names and its start from 0 to 3 if we try to put the index 4 it will give us an error.

--- encapsulation Application/encapsulation Application.cpp
+++ encapsulation Application/encapsulation Application.cpp
@@ -15,5 +15,17 @@ public:
 };
 int main()
 {
-   
+    RemoteLamp lampHouse;
+
+    lampHouse.setSwitchNo(0, "House Porch Lamp");
+    lampHouse.setSwitchNo(1, "Living Room Lamp");
+    lampHouse.setSwitchNo(2, "Bedroom Lamp");
+    lampHouse.setSwitchNo(3, "Kitchen Lamp");
+
+    cout << lampHouse.getSwitchNo(0) << endl;
+    cout << lampHouse.getSwitchNo(1) << endl;
+    cout << lampHouse.getSwitchNo(2) << endl;
+    cout << lampHouse.getSwitchNo(3) << endl;
+
+    return 0;
 }
