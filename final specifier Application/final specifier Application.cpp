ed3dbd134c627461dd1a27eb187c64ad1a0c8440
Aseel <asilalhashmi@email.com>
2026-05-19 13:09:57 +0700

    make the baseclass and virtual void function with the final key word it means that you can not override the function in the baseclass

--- final specifier Application/final specifier Application.cpp
+++ final specifier Application/final specifier Application.cpp
@@ -2,7 +2,12 @@
 
 #include <iostream>
 using namespace std;
-
+class BaseClass{
+public:
+    virtual void introduction() final {
+        cout << "Hello I am a Function from base class";
+    }
+};
 int main()
 {
  
