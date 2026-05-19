c2f68d1f892167cd5dc86562d7503fa6f169e859
Aseel <asilalhashmi@email.com>
2026-05-19 13:11:48 +0700

    we make DerivedClass inheritance  from the baseclass and try to override the function from the baseclass but it shows an error :cannot override 'final' function "BaseClass::introduction" (declared at line 7) so we have to change the name of the function

--- final specifier Application/final specifier Application.cpp
+++ final specifier Application/final specifier Application.cpp
@@ -7,6 +7,13 @@ public:
     virtual void introduction() final {
         cout << "Hello I am a Function from base class";
     }
+
+};
+class DerivedClass : public BaseClass {
+public:
+    void introduction1() {
+        cout << "Hello I am a Function from derived Class";
+    }
 };
 int main()
 {
